b2df3f1af1f54b20ea49b8fa924096f4f77c6672
alison <alisonjor@gmail.com>
2023-11-03 12:50:09 -0300

    showing a chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -17,7 +17,8 @@ class ChirpController extends Controller
     public function index(): Response 
     {
         return Inertia::render('Chirps/Index', [
-            //
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
+
         ]);
     }
 
